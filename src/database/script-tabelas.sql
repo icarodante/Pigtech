31d8d6c57e93256a1726e675491cd64f07a645b1
icarodante <icaro.menezes@sptech.school>
2022-05-03 17:05:14 -0300

    /

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -3,31 +3,18 @@
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
 
 /* para workbench - local - desenvolvimento */
-CREATE DATABASE acquatec;
+create database pigtech;
+use pigtech;
+create table cliente (
+idUser int primary key auto_increment,
+nome varchar(45),
+senha varchar(45),
+email varchar(45),
+dtCadastro datetime default current_timestamp,
+telFixo char(8),
+telCel char(9),
+cnpj char(14));
 
-USE acquatec;
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-    descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-); 
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	temperatura DOUBLE,
-	umidade DOUBLE,
-	momento DATETIME,
-	fk_aquario INT
 );
 
 
